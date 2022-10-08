4de6aa452d05463dfc1e047ede2fd78f92528838
BlueDotBrigadeFounder <noreply@BlueDotBrigade.com>
2022-10-08 12:09:58 -0400

    Test : Updated `Daten` unit tests and improved check to ensure that the appropriate default file is being loaded.

--- Tst/BlueDotBrigade.DatenLokator.TestTools-UnitTests/DatenTests.cs
+++ Tst/BlueDotBrigade.DatenLokator.TestTools-UnitTests/DatenTests.cs
@@ -1,17 +1,19 @@
 ﻿namespace BlueDotBrigade.DatenLokator.TestTools
 {
+	using System;
 	using System.Collections.Generic;
 	using System.IO;
 	using BlueDotBrigade.DatenLokator.TestsTools;
-	using BlueDotBrigade.DatenLokator.TestsTools.Configuration;
 	using BlueDotBrigade.DatenLokator.TestsTools.IO;
 	using BlueDotBrigade.DatenLokator.TestsTools.NamingConventions;
+	using BlueDotBrigade.DatenLokator.TestsTools.Reflection;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 	[TestClass]
 	public class DatenTests
 	{
-		private Lokator _lokator;
+		private const string DefaultFileName = "DefaultInput.txt";
+		private Coordinator _coordinator;
 
 		[TestInitialize]
 		public void TestInitialize()
@@ -19,15 +21,14 @@
 			var osDirectory = new OsDirectory();
 			var osFile = new OsFile();
 
-			var coordinator = new Coordinator(
+			_coordinator = new Coordinator(
+				osFile,
 				new AssertActArrange(),
 				new SubFolderThenGlobal(osDirectory, osFile),
 				new Dictionary<string, object>(),
-				"DefaultInput.txt"); 
+				DefaultFileName);
 
-			_lokator = new Lokator(osDirectory, osFile);
-			_lokator.UsingDefaultFileName("Default.txt");// duplicate
-			_lokator.Setup();
+			_coordinator.Setup(AssemblyHelper.DefaultInputFilePath);
 		}
 
 		[TestCleanup]
@@ -39,7 +40,7 @@
 		[TestMethod]
 		public void AsFilePath_FileByName_ReturnsPath()
 		{
-			var path = new Daten(_lokator).AsFilePath("FileByName.txt");
+			var path = new Daten(_coordinator).AsFilePath("FileByName.txt");
 
 			Assert.IsTrue(File.Exists(path));
 		}
@@ -47,47 +48,56 @@
 		[TestMethod]
 		public void AsFilePath_FileByConvention_ReturnsPath()
 		{
-			var path = new Daten(_lokator).AsFilePath();
+			var path = new Daten(_coordinator).AsFilePath();
 
 			Assert.IsTrue(File.Exists(path));
 		}
 
+		[TestMethod]
+		public void AsFilePath_FileByDefault_ReturnsPath()
+		{
+			var path = new Daten(_coordinator).AsFilePath(Using.DefaultFileName);
+
+			Assert.IsTrue(File.Exists(path));
+			Assert.IsTrue(path.EndsWith(DefaultFileName));
+			Assert.IsTrue(path.IndexOf("~Global", StringComparison.Ordinal) > 0);
+		}
+
+		[TestMethod]
+		public void AsString_FileByDefaultName_ReturnsCorrectContent()
+		{
+			Assert.AreEqual(
+				"Default source file shared by multiple tests.",
+				new Daten(_coordinator).AsString(Using.DefaultFileName));
+		}
 
 		[TestMethod]
 		public void AsString_FileByName_ReturnsCorrectContent()
 		{
 			Assert.AreEqual(
 				"Asked for file by name.", 
-				new Daten(_lokator).AsString("FileByName.txt"));
-		}
-
-		[TestMethod]
-		public void AsString_UsingDefaultFile_ReturnsCorrectContent()
-		{
-			Assert.AreEqual(
-				"Default source file shared by multiple tests.",
-				new Daten(_lokator).AsString(Using.DefaultFileName));
+				new Daten(_coordinator).AsString("FileByName.txt"));
 		}
 
 		[TestMethod]
 		public void AsString_FileByConvention_ReturnsCorrectContent()
 		{
 			Assert.AreEqual(
-				new Daten(_lokator).AsString(),
+				new Daten(_coordinator).AsString(),
 				"Guessed file using test method's name.");
 		}
 
 		[TestMethod]
 		public void AsString_MissingFileExtensionByConvention_ReturnsCorrectContent()
 		{
-			Assert.AreEqual("Missing file extension still works.", new Daten(_lokator).AsString());
+			Assert.AreEqual("Missing file extension still works.", new Daten(_coordinator).AsString());
 		}
 
 		[TestMethod]
 		[ExpectedException(typeof(FileNotFoundException))]
 		public void AsString_FileDoesNotExist_ThrowsFileNotFound()
 		{
-			new Daten(_lokator).AsString("ThisFileCannotBeFound.txt");
+			new Daten(_coordinator).AsString("ThisFileCannotBeFound.txt");
 		}
 	}
 }

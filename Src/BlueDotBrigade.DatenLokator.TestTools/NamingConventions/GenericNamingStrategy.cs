85d4a089004db136d76f5cf321b6f37df0a4fa35
BlueDotBrigadeFounder <noreply@BlueDotBrigade.com>
2022-10-11 07:22:59 -0400

    New : Added a `GenericNamingStrategy` #6

---
+++ Src/BlueDotBrigade.DatenLokator.TestTools/NamingConventions/GenericNamingStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace BlueDotBrigade.DatenLokator.TestsTools.NamingConventions
+{
+	public class GenericNamingStrategy : ITestNamingStrategy
+	{
+		private readonly TryParse _tryParse;
+
+		public delegate bool TryParse(string methodName, out string fileName);
+
+		public GenericNamingStrategy(TryParse tryParse)
+		{
+			_tryParse = tryParse;
+		}
+
+		public bool TryGetFileName(string methodName, out string fileName)
+		{
+			fileName = string.Empty;
+			return _tryParse(methodName, out fileName);
+		}
+	}
+}
---
+++ Tst/BlueDotBrigade.DatenLokator.TestTools-UnitTests/NamingConventions/GenericNamingStrategyTests.cs
@@ -0,0 +1,60 @@
+﻿namespace BlueDotBrigade.DatenLokator.TestTools.NamingConventions
+{
+	using BlueDotBrigade.DatenLokator.TestsTools.NamingConventions;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class GenericNamingStrategyTests
+	{
+		/// <summary>
+		/// Reprents a simple implementation of a <see cref="ITestNamingStrategy"/> delegate.
+		/// </summary>
+		/// <param name="methodName">
+		/// The unit test&apos; method name.
+		/// </param>
+		/// <param name="fileName">
+		/// Typically the delegate would return the name of the source file.
+		/// In this case, the parser simply returns the provided method name.
+		/// </param>
+		/// <returns></returns>
+		private bool PassThroughParser(string methodName, out string fileName)
+		{
+			if (string.IsNullOrWhiteSpace(methodName))
+			{
+				fileName = string.Empty;
+				return false;
+			}
+			else
+			{
+				fileName = methodName;
+				return true;
+			}
+		}
+
+		[TestMethod]
+		public void Ctor_NullMethodName_ReturnsFalse()
+		{
+			var strategy = new GenericNamingStrategy(PassThroughParser);
+
+			Assert.IsFalse(strategy.TryGetFileName(string.Empty, out _));
+		}
+
+		[TestMethod]
+		public void Ctor_ValidMethodName_ReturnsTrue()
+		{
+			var strategy = new GenericNamingStrategy(PassThroughParser);
+
+			Assert.IsTrue(strategy.TryGetFileName("DoWork", out _));
+		}
+
+		[TestMethod]
+		public void Ctor_ValidMethodName_FileNameSet()
+		{
+			var strategy = new GenericNamingStrategy(PassThroughParser);
+
+			strategy.TryGetFileName("DoWork", out var fileName);
+
+			Assert.AreEqual("DoWork", fileName);
+		}
+	}
+}

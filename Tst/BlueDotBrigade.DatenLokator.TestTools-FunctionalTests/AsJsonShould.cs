8085a09adaf160eb051ee93eb06181a35b47b748
BlueDotBrigadeFounder <noreply@BlueDotBrigade.com>
2024-11-25 21:33:03 -0500

    New : Added Daten.AsJson() unit tests. #17

---
+++ Tst/BlueDotBrigade.DatenLokator.TestTools-FunctionalTests/AsJsonShould.cs
@@ -0,0 +1,33 @@
+﻿namespace BlueDotBrigade.DatenLokator.TestTools
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	[TestClass]
+	public class AsJsonShould
+	{
+		public class TestData
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+
+		[TestMethod]
+		public void AsJson_ByConvention_ReturnsLocalFileContent()
+		{
+			// Arrange
+			var expectedData = new TestData { Id = 1, Name = "Sample" };
+
+			// Act
+			var actualData = new Daten().AsJson<TestData>();
+
+
+			// Assert
+			Assert.AreEqual(expectedData.Id, actualData.Id);
+			Assert.AreEqual(expectedData.Name, actualData.Name);
+		}
+	}
+}

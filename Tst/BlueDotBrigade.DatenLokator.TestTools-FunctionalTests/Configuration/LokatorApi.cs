8726f6dcaef77769565c7a1a5e5d428f0f31e2c8
BlueDotBrigadeFounder <noreply@BlueDotBrigade.com>
2024-11-25 20:36:10 -0500

    New : Replaced Moq with NSubstitute NuGet package 5.3.0

--- Tst/BlueDotBrigade.DatenLokator.TestTools-FunctionalTests/Configuration/LokatorApi.cs
+++ Tst/BlueDotBrigade.DatenLokator.TestTools-FunctionalTests/Configuration/LokatorApi.cs
@@ -3,7 +3,7 @@
 	using BlueDotBrigade.DatenLokator.TestTools.IO;
 	using BlueDotBrigade.DatenLokator.TestTools.NamingConventions;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
-	using Moq;
+	using NSubstitute;
 
 	/// <summary>
 	/// These methods are more about exploring the <see cref="Lokator"/> API, to ensure that the library can be extended.
@@ -14,14 +14,15 @@
 		[TestMethod]
 		public void UsingFileManagementStrategy()
 		{
-			var fakeFileManagementStrategy = new Mock<IFileManagementStrategy>().Object;
+			var fakeFileManagementStrategy = Substitute.For<IFileManagementStrategy>();
 
 			new Lokator().UsingFileManagementStrategy(fakeFileManagementStrategy);
 		}
 
+		[TestMethod]
 		public void UsingTestNamingStrategy()
 		{
-			var fakeTestNamingStrategy = new Mock<ITestNamingStrategy>().Object;
+			var fakeTestNamingStrategy = Substitute.For<ITestNamingStrategy>();
 
 			new Lokator().UsingTestNamingStrategy(fakeTestNamingStrategy);
 		}
--- Tst/BlueDotBrigade.DatenLokator.TestTools-FunctionalTests/Configuration/LokatorShould.cs
+++ Tst/BlueDotBrigade.DatenLokator.TestTools-FunctionalTests/Configuration/LokatorShould.cs
@@ -3,7 +3,8 @@
 	using BlueDotBrigade.DatenLokator.TestTools.IO;
 	using BlueDotBrigade.DatenLokator.TestTools.NamingConventions;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
-	using Moq;
+	using NSubstitute;
+	using System.Collections.Generic;
 
 	/// <summary>
 	/// These test methods are more about exploring the <see cref="Lokator"/> API, to ensure that the library can be extended.
@@ -32,19 +33,20 @@
 				{ LokatorConfiguration.RootDirectoryKey, CustomRootDirectory }
 			};
 
-			var directory = new Mock<IOsDirectory>();
-			directory.Setup(x => x.Exists(It.IsAny<string>())).Returns(true);
+			var directory = Substitute.For<IOsDirectory>();
+			directory.Exists(Arg.Any<string>()).Returns(true);
 
-			var file = new Mock<IOsFile>();
-			file.Setup(x => x.Exists(It.IsAny<string>())).Returns((string path) =>
+			var file = Substitute.For<IOsFile>();
+			file.Exists(Arg.Any<string>()).Returns((callInfo) =>
 			{
+				var path = callInfo.Arg<string>();
 				return path.StartsWith(CustomRootDirectory);
 			});
 
 			var coordinator = new Coordinator(
-				file.Object,
+				file,
 				new MemberCaseResultNamingStrategy(),
-				new SubFolderThenGlobal(directory.Object, file.Object),
+				new SubFolderThenGlobal(directory, file),
 				new Dictionary<string, object>(),
 				"Default.txt",
 				CustomRootDirectory);
